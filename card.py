040dd693b50338eb4c379a31097597fd4d0da6e7
John Ashton <John_ashton@hotmail.com>
2017-07-10 17:12:40 +0100

    add method to add suit and value to card class

--- card.py
+++ card.py
@@ -14,10 +14,47 @@ class Card(Suit, Value):
     def amount_of_cards(self):
         print "The amount of cards are: ", len(self.actual_cards)
 
-'''
+    def add_new_suit(self, suit):
+        if suit not in self.suit:
+            self.add_suit(suit)
+            self.actual_cards = list(itertools.product(self.value, self.suit))
+        else:
+            print "Suit already exists"
+
+    def add_new_value(self, value):
+        if value not in self.value:
+            self.add_value(value)
+            self.actual_cards = list(itertools.product(self.value, self.suit))
+        else:
+            print "Value already exists"
+
+"""
+    def add_new_card(self, suit, value):
+        if suit not in self.suit and value not in self.value:
+            self.add_suit(suit)
+            self.add_value(value)
+            self.actual_cards = list(itertools.product(self.value, self.suit))
+        else:
+            print "Can not add that card."
+"""
+
+
+
+
 card=Card()
 print card.value
 print card.suit
 print card.actual_cards
 print card.amount_of_cards()
-'''
+card.add_new_suit('Dinosaur')
+print card.amount_of_cards()
+print card.actual_cards
+card.add_new_value('Joker')
+print card.amount_of_cards()
+print card.actual_cards
+card.add_new_value('Ace')
+print card.amount_of_cards()
+print card.actual_cards
+card.add_new_suit('H')
+print card.amount_of_cards()
+print card.actual_cards
